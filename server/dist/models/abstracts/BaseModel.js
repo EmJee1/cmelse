7fd8420ae1d5da15cbc244764fe855a5408323bc
EmJee <67376694+EmJee1@users.noreply.github.com>
2021-12-13 18:06:51 +0100

    Removed dist folder

--- server/dist/models/abstracts/BaseModel.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class BaseModel {
-    constructor(table) {
-        this.table = table;
-    }
-    table;
-}
-exports.default = BaseModel;
