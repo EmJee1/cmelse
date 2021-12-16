f16eef8f5e9d2f0d4bd85f12d6f164613df78e9c
EmJee <67376694+EmJee1@users.noreply.github.com>
2021-12-16 19:35:19 +0100

    Added basemodel

---
+++ models/abstracts/BaseModel.ts
@@ -0,0 +1,23 @@
+interface IBaseModel {
+	endpoint?: string
+	title?: string
+}
+
+interface ICmsMetadata extends Required<IBaseModel> {
+	collection: string
+}
+
+/**
+ * All models should extend from this class.
+ * Adds cms metadata like the mongodb collection
+ */
+export default abstract class BaseModel {
+	constructor(collection: string, options: IBaseModel = {}) {
+		const endpoint = options.endpoint ?? `/${collection}`
+		const title = options.title ?? collection
+
+		this.cmsMetadata = { collection, endpoint, title }
+	}
+
+	public readonly cmsMetadata: ICmsMetadata
+}
