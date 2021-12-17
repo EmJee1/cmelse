9c62fa0e1bd1503a1e03cedf8868fdc810ceed19
EmJee <67376694+EmJee1@users.noreply.github.com>
2021-12-17 11:24:23 +0100

    Completed validateBody middleware

--- server/src/middlewares/validate-body.ts
+++ server/src/middlewares/validate-body.ts
@@ -3,19 +3,32 @@ import { Model } from 'models/interfaces/interfaces'
 import { IDatatype } from 'datatypes/interfaces/interfaces'
 import getModelProperties from '../utils/getModelProperties'
 
+/**
+ * Express middleware to validate a json request body by Joi validation set in the model.
+ * @param {Model} Model - Model parameter to check against body
+ */
 const validateBody = (model: Model) => {
 	const modelProperties = getModelProperties(model)
 
 	return (req: Request, res: Response, next: NextFunction) => {
-		const validated = modelProperties.every(property => {
-			// const value = req.body[property]
-			const prop = model[property] as IDatatype
-			console.log(prop)
+		const requestValid = modelProperties.every(property => {
+			const item = req.body[property]
+			const datatype = model[property] as IDatatype
 
-			return true
+			const result = datatype.options.validationSchema.validate(item)
+
+			// update body parameter to the parsed variant from Joi
+			// Joi automatically parses compatible types
+			// e.g. "5" > 5 if expected type is number
+			req.body[property] = result.value
+
+			return !result.error
 		})
 
-		console.log(validated)
+		if (!requestValid) {
+			res.json({ err: 'Invalid request data' }).status(400)
+			return
+		}
 
 		next()
 	}
