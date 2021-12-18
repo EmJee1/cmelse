372103721610b7b2e686ddf1bdb0b17d54c5d614
sharpTshark <71552553+sharpTshark@users.noreply.github.com>
2021-12-18 15:54:15 +0100

    frontend init

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,5 +1,16 @@
-<script setup lang="ts"></script>
-
 <template>
-	<h1>App</h1>
+	<div calss="container">
+		<navbar />
+		<router-view />
+	</div>
 </template>
+
+<script lang="ts">
+import navbar from './components/Navbar.vue'
+
+export default {
+	components: { navbar },
+}
+</script>
+
+<style lang="scss" scoped></style>
---
+++ client/src/components/Navbar.vue
@@ -0,0 +1,50 @@
+<template>
+	<div>
+		<img src="../assets/logo.svg" alt="logo" width="100" />
+		<nav>
+			<div
+				class="item"
+				v-for="(item, index) in items"
+				:key="index"
+				@click="$router.push(item.link)"
+			>
+				<h3>{{ item.name }}</h3>
+				<i :class="item.icon"></i>
+			</div>
+		</nav>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'navbar',
+	data() {
+		return {
+			items: [
+				{
+					name: 'Dashboard',
+					link: '/',
+					icon: 'fas fa-home',
+				},
+				{
+					name: 'Models',
+					link: '/models',
+					icon: 'fas fa-draw-polygon',
+				},
+				{
+					name: 'Templates',
+					link: '/templates',
+					icon: 'fas fa-file-alt',
+				},
+				{
+					name: 'Options',
+					link: '/options',
+					icon: 'fas fa-cog',
+				},
+			],
+		}
+	},
+}
+</script>
+
+<style></style>
