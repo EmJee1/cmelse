ab51f1287ef4147e3587443653f5255bdbd5461e
EmJee <67376694+EmJee1@users.noreply.github.com>
2022-01-16 00:18:31 +0100

    Add global styling and font

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,8 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import "bootstrap-icons/font/bootstrap-icons.css";
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import './style/global.scss'
 
-createApp(App)
-	.use(router)
-	.mount('#app')
+createApp(App).use(router).mount('#app')
