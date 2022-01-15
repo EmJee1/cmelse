f5563288bdd58d2dcfd2109faca581b79f2b138a
EmJee <67376694+EmJee1@users.noreply.github.com>
2022-01-15 14:57:52 +0100

    Add variables to vite scss preprocesseroptions

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,8 +1,19 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import path from 'path'
+import fs from 'fs'
+
+const scssVariables = fs.readFileSync(path.join(__dirname, './src/style/variables.scss'), 'utf-8')
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [vue(), tsconfigPaths()],
+	css: {
+		preprocessorOptions: {
+			scss: {
+				additionalData: scssVariables
+			}
+		}
+	}
 })
