024c6dfdb7fa6c819a5af4aab98972fa53eebfb3
EmJee <67376694+EmJee1@users.noreply.github.com>
2022-01-16 21:26:09 +0100

    Finished toggle datatype

--- client/src/components/datatypes/Toggle.vue
+++ client/src/components/datatypes/Toggle.vue
@@ -12,26 +12,56 @@ defineProps<{
 </script>
 
 <style lang="scss" scoped>
+$toggle-height: 40px;
+$toggle-width: 80px;
+$toggle-ball-padding: 3px;
+
 .toggle {
 	position: relative;
-	width: 100px;
-	height: 46px;
+	display: block;
+	width: $toggle-width;
+	height: $toggle-height;
+	padding: $toggle-ball-padding;
+	background-color: $white;
+	border-radius: 99px;
+	box-shadow: $shadow-gray-dark;
 
 	input {
 		opacity: 0;
 		width: 0;
 		height: 0;
+
+		&:checked + .slider {
+			background-color: #d1d1d1;
+
+			&::before {
+				transform: translateX(($toggle-width / 2));
+			}
+		}
 	}
 
-	.ball {
+	.slider {
 		position: absolute;
 		cursor: pointer;
-		top: 0;
-		left: 0;
-		bottom: 0;
-		right: 0;
+		top: $toggle-ball-padding;
+		left: $toggle-ball-padding;
+		bottom: $toggle-ball-padding;
+		right: $toggle-ball-padding;
 		background-color: $white;
-		transition: $transition;
+		transition: background-color $transition;
+		border-radius: 99px;
+
+		&::before {
+			content: '';
+			position: absolute;
+			width: $toggle-height;
+			height: $toggle-height;
+			background-color: $black;
+			transition: transform $transition;
+			border-radius: 99px;
+			top: 0;
+			left: 0;
+		}
 	}
 }
 </style>
