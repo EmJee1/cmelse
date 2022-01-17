193945f9a613b9bc114e19d9d271fa1da54321e6
EmJee <67376694+EmJee1@users.noreply.github.com>
2022-01-17 21:33:47 +0100

    Remove console logs

--- client/src/helpers/body-scroll.ts
+++ client/src/helpers/body-scroll.ts
@@ -1,11 +1,9 @@
 export const disableBodyScroll = () => {
-	console.log('disabling')
 	const body = document.querySelector('body')
 	body?.classList.add('no-scroll')
 }
 
 export const enableBodyScroll = () => {
-	console.log('enabling')
 	const body = document.querySelector('body')
 	body?.classList.remove('no-scroll')
 }
