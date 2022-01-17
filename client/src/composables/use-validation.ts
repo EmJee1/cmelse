c310c7a2135d9e1ca4cf4453356b84a6df0ba140
EmJee <67376694+EmJee1@users.noreply.github.com>
2022-01-17 18:41:11 +0100

    Fix validation not working

--- client/src/composables/use-validation.ts
+++ client/src/composables/use-validation.ts
@@ -4,8 +4,8 @@ import { AnySchema } from 'joi'
 const useValidation = (validationSchema: AnySchema) => {
 	const error = ref<string>()
 
-	const validate = (value: unknown) => {
-		const result = validationSchema.validate(value)
+	const validate = (value: FocusEvent) => {
+		const result = validationSchema.validate((value.target as HTMLInputElement).value)
 		error.value = result.error?.toString()
 	}
 
