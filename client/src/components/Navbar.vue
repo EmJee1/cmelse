fe9a3cb883b80a459b617145eff66a15f3e833bb
EmJee <67376694+EmJee1@users.noreply.github.com>
2022-01-24 18:56:13 +0100

    Navbar: add subitems

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -1,17 +1,31 @@
 <template>
-	<div class="navbar">
-		<nav>
-			<router-link to="/" class="navbar_item">
-				<i class="bi bi-house" />
-				Home
-			</router-link>
-			<router-link to="/models" class="navbar_item">
-				<i class="bi bi-collection"></i>
-				Models
-			</router-link>
-		</nav>
-	</div>
+	<nav class="navbar">
+		<router-link to="/" class="navbar_item">
+			<i class="bi bi-house" />
+			Home
+		</router-link>
+		<router-link to="/models" class="navbar_item">
+			<i class="bi bi-collection" />
+			Models
+		</router-link>
+		<ul class="navbar_subitems">
+			<li>
+				<router-link to="/models/products" class="navbar_subitem">
+					<i class="bi bi-triangle-fill" />
+					Products
+				</router-link>
+			</li>
+			<li>
+				<router-link to="/models/products" class="navbar_subitem">
+					<i class="bi bi-triangle-fill" />
+					Products
+				</router-link>
+			</li>
+		</ul>
+	</nav>
 </template>
+
+<script lang="ts" setup></script>
 
 <style lang="scss" scoped>
 .navbar {
@@ -22,15 +36,59 @@
 	bottom: 0;
 	background-color: $black;
 	color: $white;
-	padding: 30px 45px 30px 30px;
+	padding: 30px 12px 30px 30px;
+	width: 180px;
 
-	&_item {
+	&_item,
+	&_subitem {
 		color: $white;
 		text-decoration: none;
 		font-weight: 500;
 		font-size: rem(16px);
+		line-height: 1.4;
 		display: flex;
 		gap: 8px;
+		margin-bottom: 12px;
+	}
+
+	&_subitems {
+		list-style-type: none;
+		padding-left: 0;
+	}
+
+	&_subitem {
+		font-size: rem(14px);
+		position: relative;
+		transition: $transition;
+		transition-property: background-color;
+		padding: 5px 14px 5px 0;
+		border-radius: 0 80px 80px 0;
+		margin-bottom: 0;
+		gap: 14px;
+
+		& > i {
+			transform: rotate(90deg);
+			font-size: rem(8px);
+		}
+
+		&::before {
+			transition: $transition;
+			transition-property: background-color;
+			content: '';
+			position: absolute;
+			top: 0;
+			left: -100%;
+			height: 100%;
+			width: 100%;
+		}
+
+		&:hover {
+			background-color: transparentize($purple, 0.6);
+
+			&::before {
+				background-color: transparentize($purple, 0.6);
+			}
+		}
 	}
 }
 </style>
