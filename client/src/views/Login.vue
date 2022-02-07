1fc927ff5c87bbc49ba55bb3409f50bb2f7afaca
EmJee <67376694+EmJee1@users.noreply.github.com>
2022-02-07 09:56:47 +0100

    Login: update spacing and typography

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -2,7 +2,7 @@
 	<div class="login-page">
 		<main class="login-page_card">
 			<!-- Get CMS company name from configuration file -->
-			<h2 class="heading-2 login-page_brand">Brouwerij Hertog Jan</h2>
+			<h2 class="login-page_brand">Brouwerij Hertog Jan</h2>
 			<Form @submit.prevent="onSubmit">
 				<Input
 					v-model="identifier"
@@ -53,12 +53,19 @@ const onSubmit = async () => {
 		display: inline-block;
 		background-color: $gray-light;
 		border-radius: $big-radius;
-		padding: 50px 60px;
+		padding: 50px 62px;
 	}
 
 	&_brand {
 		text-align: center;
 		font-weight: 400;
+		font-size: rem(42px);
+		padding: 0 40px;
+		margin: 0;
+	}
+
+	:deep(.form) {
+		margin-top: 50px;
 	}
 }
 </style>
