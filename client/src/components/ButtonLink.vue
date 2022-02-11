dcd0a92981db978c4c484166c8d09784e83a226b
EmJee <67376694+EmJee1@users.noreply.github.com>
2022-02-11 14:56:34 +0100

    Button link: add component

---
+++ client/src/components/ButtonLink.vue
@@ -0,0 +1,39 @@
+<template>
+	<Component :is="element" class="link-btn">
+		<slot />
+	</Component>
+</template>
+
+<script lang="ts" setup>
+defineProps<{
+	element: 'a' | 'button' | 'router-link'
+}>()
+</script>
+
+<style lang="scss" scoped>
+.link-btn {
+	padding: $btn-spacing-y 0;
+	border: none;
+	background: transparent;
+	color: $black;
+	font-size: rem(14px);
+	text-decoration: none;
+	cursor: pointer;
+	position: relative;
+	transition: color $transition;
+
+	&:hover {
+		color: $purple;
+	}
+
+	&:after {
+		content: '';
+		display: block;
+		height: 1px;
+		width: 100%;
+		margin-top: 2px;
+		position: absolute;
+		background: $purple;
+	}
+}
+</style>
