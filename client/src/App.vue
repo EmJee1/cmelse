3d6ee10ff6b10c25868a42332bee5ff10175b129
EmJee <67376694+EmJee1@users.noreply.github.com>
2022-02-11 15:23:42 +0100

    Navbar: fix visibility

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,3 +1,19 @@
 <template>
-	<router-view />
+	<Navbar v-if="loggedInUser" />
+	<div :class="{ 'logged-in': loggedInUser }">
+		<router-view />
+	</div>
 </template>
+
+<script lang="ts" setup>
+import loggedInUser from './stores/user-store'
+import Navbar from './components/Navbar.vue'
+</script>
+
+<style lang="scss" scoped>
+.logged-in {
+	@media ($mq-tablet) {
+		margin-left: $navbar-width;
+	}
+}
+</style>
