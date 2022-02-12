31a51d33a640786ae549ab073bf6cc14439f67c8
EmJee <67376694+EmJee1@users.noreply.github.com>
2022-02-12 15:16:59 +0100

    SVG: add svg-loader to vite config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import svgLoader from 'vite-svg-loader'
 import path from 'path'
 import fs from 'fs'
 
@@ -9,7 +10,7 @@ const scssMixins = fs.readFileSync(path.join(__dirname, './src/style/mixins.scss
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [vue(), tsconfigPaths()],
+	plugins: [vue(), tsconfigPaths(), svgLoader()],
 	css: {
 		preprocessorOptions: {
 			scss: {
