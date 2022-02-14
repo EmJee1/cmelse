a53cc40b112ead353210891f95c89ce6815c226d
EmJee <67376694+EmJee1@users.noreply.github.com>
2022-02-14 19:00:19 +0100

    Get localized message: add function

---
+++ server/src/utils/get-localized-message.ts
@@ -0,0 +1,28 @@
+import LocaleMessages from '../i18n/locale'
+import logger from '../config/winston'
+
+const getLocalizedMessage = (locales: { [key: string]: LocaleMessages }, locale: string, message: string) => {
+	if (!locales[locale]) {
+		logger.warn(`Localization locale ${locale} is not supported`)
+		throw new Error(`Unknown locale ${locale}`)
+	}
+
+	const selectedLocale = locales[locale]
+	const messageParts = message.split('.')
+	const localizedMessage: string = messageParts.reduce((object, key) => {
+		// TODO: figure out why typescript is complaining here
+		// @ts-ignore
+		const part = object[key]
+
+		if (!part) {
+			logger.warn(`Localization object key ${key} does not exist on locale; full path: ${message}`)
+			throw new Error(`Object key ${key} does not exist on locale`)
+		}
+
+		return part
+	}, selectedLocale)
+
+	return localizedMessage
+}
+
+export default getLocalizedMessage
