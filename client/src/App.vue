1131a3404d19ec3e00508fb91d85606ffc4fca53
EmJee <67376694+EmJee1@users.noreply.github.com>
2022-02-18 00:56:07 +0100

    App: login user if he has a stored valid token

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,15 +1,41 @@
 <template>
-	<Navbar v-if="loggedInUser" />
-	<Login v-if="!loggedInUser" />
-	<div v-else class="app">
-		<router-view />
-	</div>
+	<Loader v-if="loading" fullscreen />
+	<template v-else>
+		<Navbar v-if="loggedInUser" />
+		<Login v-if="!loggedInUser" />
+		<div v-else class="app">
+			<router-view />
+		</div>
+	</template>
 </template>
 
 <script lang="ts" setup>
+import axios from 'axios'
+import { onMounted, ref } from 'vue'
 import loggedInUser from './stores/user-store'
 import Login from './views/Login.vue'
 import Navbar from './components/Navbar.vue'
+import Loader from './components/Loader.vue'
+
+const loading = ref(false)
+
+onMounted(() => {
+	if (localStorage.getItem('token')) {
+		loading.value = true
+
+		axios
+			.get('/authentication/profile')
+			.then(({ data }) => {
+				loggedInUser.value = data
+			})
+			.catch(() => {
+				localStorage.removeItem('token')
+			})
+			.finally(() => {
+				loading.value = false
+			})
+	}
+})
 </script>
 
 <style lang="scss" scoped>
