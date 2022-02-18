64d1b6268484f1d6b113fa0f1a74fc179f4a0d76
EmJee <67376694+EmJee1@users.noreply.github.com>
2022-02-18 15:56:10 +0100

    Card: add component

---
+++ client/src/components/Card.vue
@@ -0,0 +1,15 @@
+<template>
+	<div class="container">
+		<div class="card">
+			<slot />
+		</div>
+	</div>
+</template>
+
+<style lang="scss" scoped>
+.card {
+	background: $white;
+	box-shadow: $shadow;
+	border-radius: $big-radius;
+}
+</style>
