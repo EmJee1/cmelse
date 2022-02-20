5353e2b5820d34409a2810bb36abe986685eb1df
EmJee <67376694+EmJee1@users.noreply.github.com>
2022-02-20 14:25:29 +0100

    Table: add custom column names support

--- client/src/components/Table.vue
+++ client/src/components/Table.vue
@@ -2,15 +2,15 @@
 	<table class="table" v-if="data">
 		<thead class="table_head">
 			<tr class="table_row">
-				<th v-for="column in columns" :key="column" class="table_data">
-					{{ column }}
+				<th v-for="column in computedColumns" :key="column" class="table_data">
+					{{ column.displayName }}
 				</th>
 			</tr>
 		</thead>
 		<tbody class="table_body">
 			<tr v-for="(row, index) in data" :key="index" class="table_row">
-				<td v-for="column in columns" :key="column" class="table_data">
-					{{ row[column] }}
+				<td v-for="column in computedColumns" :key="column.key" class="table_data">
+					{{ row[column.key] }}
 				</td>
 			</tr>
 		</tbody>
@@ -18,19 +18,37 @@
 </template>
 
 <script lang="ts" setup>
-import { computed } from 'vue'
+import { computed, watch } from 'vue'
+
+interface IColumns {
+	displayName: string
+	key: string
+}
 
 const props = defineProps<{
 	data?: { [key: string]: unknown }[]
+	columns?: IColumns[]
 }>()
 
-const columns = computed(() => {
+const computedColumns = computed<null | IColumns[]>(() => {
+	if (props.columns) {
+		return props.columns
+	}
+
 	if (!props.data) {
 		return null
 	}
 
-	return Object.keys(props.data[0])
+	return Object.keys(props.data[0]).map(key => ({
+		displayName: key,
+		key,
+	}))
 })
+
+watch(
+	() => computedColumns.value,
+	curr => console.log(curr)
+)
 </script>
 
 <style lang="scss" scoped>
